9a5d0b035af1a578776bcff0a813c81536fcad47
AliGadoo <aligado232@gmail.com>
2025-09-19 19:07:05 +0300

    fix responsive design and fix when reload the page label position

--- Task-2-Contact-Form/main.js
+++ Task-2-Contact-Form/main.js
@@ -4,6 +4,12 @@ fields.forEach((input) => {
   const wrapper = input.closest(".field, .textarea_field");
   const label = wrapper.querySelector("label");
 
+  if (input.value.trim() !== "") {
+    input.tagName === "INPUT"
+      ? label.classList.add("move_label_up")
+      : label.classList.add("textarea_label_up");
+  }
+
   input.addEventListener("focusin", () => {
     input.tagName == "INPUT"
       ? label.classList.add("move_label_up")
