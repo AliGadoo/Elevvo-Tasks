7b31f0d843e6d2d7dc908bc1e689c1f0ea461120
AliGadoo <aligado232@gmail.com>
2025-09-24 01:17:24 +0300

    implement features Card

--- Task-3-Task-App/src/App.jsx
+++ Task-3-Task-App/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FeatureCard from "./Components/FeatureCard";
 import taskManagerIcon from "/taskManagerIcon.svg";
+import calendarIcon from "/calendar.svg";
+import usersIcon from "/users.svg";
 
 const App = () => {
   return (
@@ -12,13 +14,33 @@ const App = () => {
         </p>
         <button className="btn-primary mt-4">Get Started</button>
       </header>
-      <main>
-        <FeatureCard
-          icon={taskManagerIcon}
-          title={"Easy Task Management"}
-          desc={"Organize tasks quickly and simply"}
-        />
+      <main className="max-w-7xl mx-auto flex justify-center px-4">
+        <div className="mt-16">
+          <h2 className="text-xl font-semibold text-center mb-8">
+            Why TaskFlow?
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <FeatureCard
+              icon={taskManagerIcon}
+              title={"Organize Tasks"}
+              desc={"Organize tasks quickly and simply"}
+            />
+            <FeatureCard
+              icon={calendarIcon}
+              title={"Calendar View"}
+              desc={"Keep track of tasks by date"}
+            />
+            <FeatureCard
+              icon={usersIcon}
+              title={"Work Together"}
+              desc={"Organize tasks quickly and simply"}
+            />
+          </div>
+        </div>
       </main>
+      {/* <!-- Team Collaboration --> */}
+
+      {/* Keep track of tasks by date */}
       <footer></footer>
     </div>
   );
--- Task-3-Task-App/src/Components/FeatureCard.jsx
+++ Task-3-Task-App/src/Components/FeatureCard.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 const FeatureCard = ({ icon, title, desc }) => {
   return (
-    <div className="flex flex-col items-center ">
-      <div>
+    <div className="min-w-[280px] text-center bg-white shadow-xl rounded-xl p-8 hover:translate-y-[-4px] transition">
+      <div className="flex justify-center mb-4">
         <img src={icon} alt={`icon for ${title}`} />
       </div>
-      <h2>{title}</h2>
-      <p>{desc}</p>
+      <h2 className="text-xl font-bold text-gray-950">{title}</h2>
+      <p className="text-sm text-gray-600 mt-2">{desc}</p>
     </div>
   );
 };
